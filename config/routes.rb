b509917925cabf0435ac5072e3a8752178ad6654
Student <example@example.com>
2020-01-22 15:36:19 -0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "accounts#new"
+
+  get '/signup' => 'accounts#new'
+  post '/accounts' => 'accounts#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
+  resources :images
+  resources :accounts do
+    resources :images
+  end
 end
